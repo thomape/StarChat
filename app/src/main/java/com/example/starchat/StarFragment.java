88dd086a100697810f6796f66fea83b603958db3
terrington <tom.errington58@gmail.com>
2019-09-20 15:55:49 -0400

    Added some pictures and tried to add a scroll view to the bottom of the page to view images.

--- app/src/main/java/com/example/starchat/StarFragment.java
+++ app/src/main/java/com/example/starchat/StarFragment.java
@@ -1,18 +1,67 @@
 package com.example.starchat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class StarFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_star, null);
+
+        View view = inflater.inflate(R.layout.fragment_star, container, false);
+
+        // Button for ankle
+        ImageButton ankle = (ImageButton) view.findViewById(R.id.ankle);
+        ankle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
+                startActivity(intent);
+            }
+        });
+
+        // Button for Indom
+        ImageButton indom = (ImageButton) view.findViewById(R.id.indom);
+        indom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
+                startActivity(intent);
+            }
+        });
+
+        // Button for trex
+        ImageButton trex = (ImageButton) view.findViewById(R.id.trex);
+        trex.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
+                startActivity(intent);
+            }
+        });
+
+        // Button for trike
+        ImageButton trike = (ImageButton) view.findViewById(R.id.trike);
+        trike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
+                startActivity(intent);
+            }
+        });
+
+        return view;
+
     }
 }
